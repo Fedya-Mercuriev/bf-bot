e2f3aeaaf4050c06756171a0a2e86fce73a15443
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-01-11 14:14:54 +0700

    Time validation done

--- assets/order/validate/validate-time/time.js
+++ assets/order/validate/validate-time/time.js
@@ -13,15 +13,16 @@ const timeValidation = new Scene('timeValidation');
 class Time {
     constructor() {
         this.today = new Date();
-        this.time = undefined;
+        this.validatedTime = undefined;
+        this.workingHours = {};
     }
 
-    get getTime() {
-        return this.time;
+    get time() {
+        return this.validatedTime;
     }
 
-    set setTime(time) {
-        this.time = time;
+    set time(time) {
+        this.validatedTime = time;
     }
 
     _hasDateAndShipping(date, shipping) {
@@ -34,71 +35,227 @@ class Time {
         })
     }
 
-    _isToday(date) {
-        return new Promise((resolve, reject) => {
-            if (this.today.getFullYear() === date.getFullYear() && this.today.getMonth() === date.getMonth() && this.today.getDate() === date.getDate()) {
-                resolve(date);
-            } else {
-                reject(date);
-            }
-        })
-    }
-
-    _isWeekend(date) {
-            if (date.getDay() === 6 || date.getDay() === 0) {
-                return true;
-            } else {
-                return false;
-            }
-    }
-
     _getWorkingHours(date) {
-        let workingHours = {start: 0, end: 0};
-        if (date.getDay() === 6 || date.getDay() === 0) {
-            let { start, end } = Contacts.workingHours.weekdays;
+        let workingHours = {start: 0, finish: 0},
+            usedDate = new Date(date);
+        if (usedDate.getDay() === 6 || usedDate.getDay() === 0) {
+            let { start, finish } = Contacts.workingHours.weekends;
             workingHours.start = start;
-            workingHours.end = end;
+            workingHours.finish = finish;
             return workingHours;
         } else {
-            let { start, end } = Contacts.workingHours.weekends;
+            let { start, finish } = Contacts.workingHours.weekdays;
             workingHours.start = start;
-            workingHours.end = end;
+            workingHours.finish = finish;
+            this.workingHours = workingHours;
             return workingHours;
         }
     }
 
+    _isToday(date) {
+        let usedDate = new Date(date);
+        if (usedDate.getFullYear() === this.today.getFullYear() && usedDate.getMonth() === this.today.getMonth() && usedDate.getDate() === this.today.getDate()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     requestTime(ctx) {
-        let { orderDate, shipping } = order.getOrderInfo;
+        let { orderDate, shipping } = order.getOrderInfo,
+            estimatedTime = 2400000;
             // closestTime = this._calculateClosestTime(),
             // hours = closestTime.getHours(),
             // minutes = closestTime.getMinutes();
         console.log(`${orderDate} : ${shipping}`);
         this._hasDateAndShipping(orderDate, shipping).then(
             () => {
-                ctx.reply(`Введите самостоятельно желаемое время, когда хотите забрать букет.\n⚠️Пожалуйста, пишите время в формате: ЧЧ:ММ.`);
+                if (shipping !== false) {
+                    estimatedTime = 5400000;
+                }
+                let { start, finish } = this._getWorkingHours(orderDate);
+                ctx.reply(`Введите самостоятельно желаемое время, когда хотите забрать букет.\n⚠️Пожалуйста, пишите время в формате: ЧЧ:ММ.\n⚠С учетом выбранного вами способа доставки нам потребуется ${new Date(estimatedTime).getMinutes()} мин., чтобы сделать букет. Имейте это ввиду когда будете указывать время\n🗓 Сегодня мы работаем с ${start}:00 до ${finish}:00`);
             }).catch((error) => {
                 ctx.reply(`${error.message}`, Markup.inlineKeyboard(
                     [Markup.callbackButton('В меню заказа', 'menu')]
                 ).extra());
             });
-        // if (this._isToday(orderDate)) {
-        //     ctx.reply("Введите самостоятельно желаемое время, когда хотите забрать букет; или выберите предложенное время ниже");
-        // }
+    }
+
+    identifyTime(timeString) {
+        return new Promise((resolve, reject) => {
+            let timeArray;
+            if (timeString.match(/(\d+)[\s\/.,:\\-](\d+)/g) !== null) {
+                // Упакуем данные (часы и минуты) в массив и проверим
+                timeArray = timeString.split(/[\s\/.,:\\-]/);
+                for (let data in timeArray) {
+                    if (isNaN(data)) {
+                        // если тип данных не числовой, то выводим ошибку
+                        reject(new Error("⛔️ Пожалуйста, введите корректное время"));
+                    }
+                }
+                resolve(timeArray);
+            } else {
+                reject(new Error("⛔️ Пожалуйста, введите корректное время"));
+            }
+        });
+    }
+
+    _checkHours(date, timeToCompareWith) {
+        return new Promise((resolve, reject) => {
+            // date = введенные часы
+            if (date.getHours() > timeToCompareWith.getHours() && date < finishWork - estTime) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+
+    // Высчитывает временные рамки, за которые нельзя заходить (с учетом выбранного типа доставки)
+    _calculateTimeLimits(orderDate, workTime, estTime) {
+        // orderDate = ms; estTime = ms; workTime = ms;
+        // return (workTime * 3600000) + estTime + orderDate;
+        return workTime + estTime;
+    }
+
+    // Определяет успеет ли компания сделать и доставить букет
+    _compareTime(targetTime, workingHours) {
+        // targetTime - введенное время, workingHours - временные рамки (начало или конец работы)
+        // usedTime = ms; start = ms; finish = ms; shipping = boolean
+        let {start, finish} = workingHours,
+            startWork = new Date(start),
+            finishWork = new Date(finish),
+            // Относительно медианы будем сравнивать введенноевремя с началом работы, либо с окончанием
+            median = Math.floor((finishWork.getHours() - startWork.getHours()) / 2);
+        let difference = start - targetTime;
+        return new Promise((resolve, reject) => {
+            if (new Date(targetTime).getHours() < new Date(start).getHours() + median) {
+                // Сравниваем с стартом
+                if (start - targetTime <= 0) {
+                    resolve();
+                } else {
+                    reject();
+                }
+
+            } else {
+                // Сравниваем с финишем
+                if (finish - targetTime >= 0) {
+                    resolve();
+                } else {
+                    reject();
+                }
+            }
+        });
+    }
+
+    // Проверяет введенные часы
+    checkTime(timeArray) {
+        let [hours, minutes] = timeArray,
+            { start, finish } = this.workingHours,
+            { shipping, orderDate } = order.getOrderInfo,
+            // По умолчанию стоит 40 мин
+            estimatedTime = 2400000,
+            makeErrorMsg = (time) => {
+                return `⛔ К сожалению, мы не успеем сделать букет к указанному вами времени. С учетом выбранного вами способа доставки нам потребуется ${time} минут.\nПожалуйста, укажите другое время, другой способ доставки или другую дату`
+            };
+
+        return new Promise((resolve, reject) => {
+            // Создаем переменную, с которой будем сравнивать начало и конец работы вместе с прибавленным или вычитанным значением необходимого
+            // для создания и доставки букета времени
+            // Формат: ms
+            let usedTime = orderDate + (hours * 3600000) + (minutes * 60000),
+                startWork = new Date(orderDate),
+                finishWork = new Date(orderDate);
+
+            // Была выбрана доставка
+            if (shipping !== false) {
+                // Преобразуем данные о доставке к логическому типу для удобства
+                shipping = true;
+
+                // Требуемое время 90 мин
+                estimatedTime = 5400000;
+                // Записываем время начала и конца работы как полноценную дату для удобства сравнения
+                // start = ms; finish = ms
+
+                // Если в качестве даты заказа был выбран сегодняшний день, тогда устанавливаем текущие часы и минуты как начало работы,
+                // чтоб пользователь не смог заказать букет на время, которое уже прошло
+                if (this._isToday(orderDate) && new Date().getHours() > start) {
+                    start = this._calculateTimeLimits(orderDate, Date.parse(new Date().toString()), estimatedTime);
+                } else {
+                    // StartWork - переменная, хранящая дату вместе со временем начала работы (в качестве года, месяца и дня используются данные из заказа)
+                    startWork.setHours(start);
+                    startWork.setMinutes(0);
+                    start = this._calculateTimeLimits(orderDate, Date.parse(startWork.toString()), estimatedTime);
+                }
+                finishWork.setHours(finish);
+                finishWork.setMinutes(0);
+                // Для финиша (с доставкой) временные рамки не установлены - ограничение стоит по умолчанию: время окончания работы
+                finish = this._calculateTimeLimits(orderDate, Date.parse(finishWork.toString()), 0);
+
+                // После проверки указанного времени либо выдаем положительный результат, либо ошибку характерную для определенного типа доставки
+                this._compareTime(usedTime, {start: start, finish: finish}, shipping).then(() => {
+                    resolve(usedTime);
+                }, () => {
+                    reject(new Error(makeErrorMsg(90)));
+                });
+
+            // Был выбран самовывоз
+            } else {
+                if (this._isToday(orderDate) && new Date().getHours() > start) {
+                    start = this._calculateTimeLimits(orderDate, Date.parse(new Date().toString()), estimatedTime);
+                } else {
+                    startWork.setHours(start);
+                    startWork.setMinutes(0);
+                    start = this._calculateTimeLimits(orderDate, Date.parse(startWork.toString()), estimatedTime);
+                }
+                finishWork.setHours(finish);
+                finishWork.setMinutes(0);
+                // Для финиша (с самовывозом) временные рамки установлены как - 10 минут от конца работы, чтоб клиент успел прийти за букетом
+                finish = this._calculateTimeLimits(orderDate, Date.parse(finishWork.toString()), -600000);
+                // Требуемое время 40 мин
+                this._compareTime(usedTime, {start: start, finish: finish}, shipping).then(() => {
+                    resolve(usedTime);
+                }, () => {
+                    reject(new Error(makeErrorMsg(40)));
+                });
+            }
+        });
     }
 
     validateTime(ctx, timeString = null) {
         let { orderDate } = order.getOrderInfo,
-            workingHours = this._getWorkingHours(orderDate);
-        // Разобьем строку со временем на части
-        let timeArray = timeString.match('/()/g')
-        ctx.reply(`Проверяем время: ${time}`);
+            timeArray;
+        this.workingHours = this._getWorkingHours(orderDate);
+
+        // if (this.today.getDate() === new Date(orderDate).getDate() && this.today.getMonth() === new Date(orderDate).getFullYear() && this.today.getMonth() === new Date(orderDate).getFullYear()) {
+        //     console.log("• Выбранная дата: сегодня •");
+        //     this.workingHours.start = new Date().getHours();
+        // }
+        // Проверим валидность строки и подготовим ее к проверке
+        this.identifyTime(timeString)
+            .then((result) => {
+                return this.checkTime(result);
+            })
+            .then((result) => {
+                this.time = result;
+                console.log(`Текущее выбранное время: ${this.time}`);
+                ctx.reply(`✅ Хорошо, букет будет готов к ${new Date(result).getHours()}:${new Date(result).getMinutes()}`).then(() => {
+                    ServiceOps.requestContinue(ctx, "введите другое время");
+                });;
+            }, (error) => {
+                ctx.reply(error.message);
+            })
+            .catch((error) => {
+                ctx.reply(error.message);
+            });
     }
 }
 
-const time = new Time();
+const validateTime = new Time();
 
 timeValidation.enter((ctx) => {
-    time.requestTime(ctx);
+    validateTime.requestTime(ctx);
 });
 
 timeValidation.on('callback_query', (ctx) => {
@@ -107,15 +264,23 @@ timeValidation.on('callback_query', (ctx) => {
    //     ctx.reply("Нажата кнопка \"Продолжить\"");
    // }
     if (ctx.update['callback_query'].data === 'menu') {
+        order.displayInterface(ctx);
+        ctx.scene.leave();
+    } else {
+        // Обработать кнопку "Продолжить"
+        order.setOrderInfo = ['orderTime', validateTime.time];
+        let { orderTime } = order.getOrderInfo;
+        console.log(`Время заказа: ${orderTime}`);
+        ctx.telegram.deleteMessage(ctx.update['callback_query'].message.chat.id, ctx.update['callback_query'].message['message_id']);
         order.displayInterface(ctx, `Выберите любой пункт в меню и следуйте инструкциям.
             \nПри правильном заполнении данных напротив выбранного пукта меня будет стоять ✅`);
-        ctx.scene.leave();
+        ctx.scene.leave('timeValidation');
     }
 });
 
 timeValidation.on('message', (ctx) => {
-    time.validateTime(ctx, ctx.update.message.text);
+    validateTime.validateTime(ctx, ctx.update.message.text);
 });
 
-module.exports = time;
+module.exports = validateTime;
 module.exports = timeValidation;
