6af115ae874d66fcffdc3278c0eaeaf1a5e8c448
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-03-25 17:44:00 +0700

    Fixed date validation pattern
    Now identifies date despite multiple spaces or other permitted
    characters between desired date values.

--- assets/order/validate/validate-date/chunks/identify-date.js
+++ assets/order/validate/validate-date/chunks/identify-date.js
@@ -3,13 +3,13 @@ function identifyDate(string) {
     return new Promise((resolve, reject) => {
         let result;
 
-        if (string.match(/(\d{1,2})[\s\/.,\-]([а-яё]+)/i)) {
+        if (string.match(/(\d{1,2})[\s\/.,\-]{0,}([а-яё]+)/i)) {
             result = string.match(/(\d{1,2})[\s\/.,\-]?([а-яё]+)/i);
             result = result.splice(1, 2);
             // Результат: ["день", "месяц"]
             resolve(result);
 
-        } else if (string.match(/(\d{1,2})[\s\/.,:\\-](\d{1,2})/i)) {
+        } else if (string.match(/(\d{1,2})[\s\/.,:\\-]{0,}(\d{1,2})/i)) {
             result = string.match(/(\d{1,2})[\s\/.,:\\-](\d{1,2})/i);
             // На данном этапе result выглядит так: ["26.06"]
             result = result.splice(1, 2);
