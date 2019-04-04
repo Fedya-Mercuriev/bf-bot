955f4ca37509367ea52ea400f9c81877e964c779
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-04-05 03:11:27 +0700

    Added a few methods to base class, fixed te others

--- assets/order/base-class.js
+++ assets/order/base-class.js
@@ -1,13 +1,17 @@
+const Telegraf = require('telegraf');
+const { Markup, Extra } = Telegraf;
+const Contacts = require('../main-page/contacts');
+
 class Base {
     constructor() {
         this._userMessages = [];
     }
 
-    static get userMessages() {
+    get _messagesToDelete() {
         return this._userMessages;
     }
 
-    static set userMessages(message) {
+    set _messagesToDelete(message) {
         if (message === 'delete') {
             this._userMessages.length = 0;
         } else {
@@ -15,6 +19,23 @@ class Base {
             this._userMessages.push(id);
         }
     }
+
+    async _requestContinue(ctx, additionalMsg) {
+        return ctx.reply(`Нажмите на кнопку ниже, чтобы продолжить заказ букета или ${additionalMsg}`,
+            Markup.inlineKeyboard([
+                Markup.callbackButton('Продолжить', '_saveAndExit')
+            ]).extra())
+    }
+
+    returnToMenu(ctx, sceneName) {
+        ctx.scene.leave(sceneName);
+    }
+
+    displayPhoneNumber(ctx) {
+        return Contacts.showPhoneNumber(ctx);
+    }
+
+
 }
 
 module.exports = Base;
