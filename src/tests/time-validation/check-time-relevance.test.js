f4842f2390f48e4e93427dc3c5a5a8df979065b5
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-04-11 02:58:01 +0700

    Added tests for check-time-relevance

--- src/tests/time-validation/check-time-relevance.test.js
+++ src/tests/time-validation/check-time-relevance.test.js
@@ -5,5 +5,15 @@ const identifyTime = require('./../../order/validate/validate-time/chunks/identi
 const checkTimeRelevance = require('./../../order/validate/validate-time/chunks/check-time-relevance');
 
 describe('Testing time-relevance check', () => {
-
+    test.each(['24, 00', '25, 00', '246, 00', '2412, 00'])
+        ('(Given string -> (\'%s\')) Throws an error if hour in given strign is greater tahn 23',
+            (timeString) => {
+                return identifyTime(timeString)
+                    .then((identifiedTime) => {
+                        return checkTimeRelevance(identifiedTime);
+                    })
+                    .catch((e) => {
+                        expect(e.message).toMatch('⛔️ Пожалуйста, введите корректное время!');
+                    });
+            });
 });
