dbb4c480950f9ccf98fee26745728a08c5e9323e
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-04-16 18:19:01 +0700

    Added scene for date validation

---
+++ src/scenes/validate-date.js
@@ -0,0 +1,57 @@
+/* eslint-disable indent */
+/* eslint-disable no-underscore-dangle */
+const Telegraf = require('telegraf');
+const { Markup } = Telegraf;
+const session = require('telegraf/session');
+const Stage = require('telegraf/stage');
+const Scene = require('telegraf/scenes/base');
+const { leave } = Stage;
+const order = require('./../order/order');
+const validateDate = require('./../order/validate/validate-date/date');
+const dateValidation = new Scene('dateValidation');
+
+// Команды для сцены
+dateValidation.enter(async(ctx) => {
+    ctx.telegram.answerCbQuery(ctx.update.callback_query.id, '⏳ Загружаю необходимые компоненты...');
+    let { orderDate } = order.orderInfo;
+    validateDate._messagesToDelete = await ctx.reply('Давайте проверим дату',
+        Markup.keyboard([
+            ['📜 Меню заказа'],
+            ['📞 Связаться с магазином'],
+            ['⛔ Отменить заказ'],
+        ])
+        .oneTime()
+        .resize()
+        .extra()
+    );
+    if (orderDate !== undefined) {
+        orderDate = validateDate.russifyDate(new Date(orderDate));
+        validateDate.confirmDateOverride(ctx, orderDate);
+    } else {
+        validateDate.requestDate(ctx);
+    }
+});
+
+dateValidation.on('message', async(ctx) => {
+    if (ctx.updateSubTypes[0] !== 'text') {
+        validateDate._messagesToDelete = await ctx.reply('⛔️ Пожалуйста, отправьте дату в виде текста');
+    } else if (ctx.update.message.text.match(/меню заказа/i)) {
+        validateDate.returnToMenu(ctx, order.displayInterface.bind(order), 'dateValidation');
+    } else if (ctx.update.message.text.match(/связаться с магазином/i)) {
+        validateDate.displayPhoneNumber(ctx);
+    } else if (ctx.update.message.text.match(/отменить заказ/i)) {
+        ctx.reply('Отменяем заказ (пока нет)');
+    } else {
+        validateDate._validateDate(ctx, ctx.message.text);
+    }
+});
+
+dateValidation.on('callback_query', (ctx) => {
+    try {
+        validateDate.invokeFunction(ctx.update.callback_query.data, ctx);
+    } catch (error) {
+        ctx.telegram.answerCbQuery(ctx.update.callback_query.id, '⛔ Cейчас эта кнопка не доступна!');
+    }
+});
+
+module.exports = dateValidation;
