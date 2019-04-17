fd7286065150ac4889ebf0e37d2b2163ef488ca9
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-04-17 19:52:30 +0700

    Minor fixes to time scene

---
+++ src/scenes/time-scene.js
@@ -0,0 +1,60 @@
+/* eslint-disable indent */
+const Telegraf = require('telegraf');
+const { Markup, Extra } = Telegraf;
+const session = require('telegraf/session');
+const Stage = require('telegraf/stage');
+const Scene = require('telegraf/scenes/base');
+const order = require('../order/order');
+const validateTime = require('./../order/validate/validate-time/time');
+const { leave } = Stage;
+
+const timeValidation = new Scene('timeValidation');
+
+timeValidation.enter(async(ctx) => {
+    ctx.telegram.answerCbQuery(ctx.update.callback_query.id, '⏳ Загружаю необходимые компоненты...');
+    const { orderTime } = order.orderInfo;
+    const message = await ctx.reply('Давайте проверим время',
+        Markup.keyboard([
+            ['📜 Меню заказа'],
+            ['📞 Связаться с магазином'],
+            ['⛔ Отменить заказ'],
+        ])
+        .oneTime()
+        .resize()
+        .extra());
+    validateTime.messages = {
+        messageType: 'other',
+        messageObj: message,
+    };
+    if (orderTime !== undefined) {
+        validateTime.confirmTimeOverwrite(ctx, orderTime);
+    } else {
+        validateTime.requestTime(ctx);
+    }
+});
+
+timeValidation.on('callback_query', (ctx) => {
+    try {
+        validateTime.invokeFunction(ctx.update.callback_query.data, ctx);
+    } catch (error) {
+        ctx.telegram.answerCbQuery(ctx.update.callback_query.id, '⛔ Cейчас эта кнопка не доступна!');
+    }
+});
+
+timeValidation.on('message', (ctx) => {
+    if (ctx.updateSubTypes[0] === 'text') {
+        const messageText = ctx.update.message.text;
+        if (messageText.match(/Меню заказа/i)) {
+            validateTime.returnToMenu(ctx, 'timeValidation');
+        } else if (messageText.match(/Связаться с магазином/i)) {
+            validateTime.displayPhoneNumber(ctx);
+        } else if (messageText.match(/Отменить заказ/i)) {
+            ctx.reply('Отменяем заказ (пока нет)');
+        } else {
+            validateTime.validateTime(ctx, messageText);
+        }
+    }
+});
+
+// module.exports = validateTime;
+module.exports = timeValidation;
