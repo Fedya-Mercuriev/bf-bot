90a40f2ca176fc4a92d56cbebff19e6aef9bcf33
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-04-19 03:48:43 +0700

    Added invoice generator

--- src/order/invoice.js
+++ src/order/invoice.js
@@ -1,26 +1,31 @@
-const config = require('../config');
-
-module.exports = class Invoice {
-    constructor(title, descr, img, price) {
-        this.purchaseData = {
-            provider_token: config.payment_token,
-            start_parameter: 'time-machine-sku',
-            title: title,
-            description: descr,
-            currency: 'RUB',
-            photo_url: img,
-            is_flexible: true,
-            prices: [
-                { label: title, amount: price },
-                { label: 'Gift wrapping', amount: 110 }
-            ],
-            payload: {
-                coupon: 'BLACK FRIDAY'
-            }
-        };
+/* eslint-disable indent */
+function generateInvoice(options) {
+    const { bouquet: bouquetInfo, shipping } = options;
+    const { name, description: bouquetDescription, photo, price } = bouquetInfo;
+    const result = {
+        provider_token: process.env.PAYMENT_TOKEN,
+        start_parameter: 'bouquet-invoice',
+        title: name,
+        description: bouquetDescription,
+        currency: 'RUB',
+        photo_url: photo,
+        is_flexible: false,
+        photo_width: 320,
+        photo_height: 320,
+        prices: [
+            { label: 'Букет', amount: price * 100 },
+        ],
+        payload: {
+            coupon: 'BLACK FRIDAY',
+        },
+    };
+    if (shipping !== false) {
+        result.prices.push({
+            label: 'Доставка',
+            amount: (300 * 100),
+        });
     }
+    return result;
+};
 
-    show() {
-        // Вернем чек
-    }
-}
+module.exports = generateInvoice;
